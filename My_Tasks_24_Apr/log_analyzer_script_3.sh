53218cf90cc9ccad966af3988fbff6083ce6fc41
samarthf-devops <samarthf@valueaddsofttech.com>
2026-04-25 11:10:11 +0530

    Update and rename log_analyzer_script4.sh to log_analyzer_script_3.sh

--- My_Tasks_24_Apr/log_analyzer_script_3.sh
+++ My_Tasks_24_Apr/log_analyzer_script_3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if log file is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <log_file>"
     exit 1
@@ -8,24 +7,20 @@ fi
 
 LOG_FILE="$1"
 
-# Check if file exists
 if [ ! -f "$LOG_FILE" ]; then
     echo "Error: File not found!"
     exit 1
 fi
 
-# Count ERROR and INFO
 ERROR_COUNT=$(grep -c "ERROR" "$LOG_FILE")
 INFO_COUNT=$(grep -c "INFO" "$LOG_FILE")
 
-# Output format
 echo "ERROR: $ERROR_COUNT"
 echo "INFO: $INFO_COUNT"
 
 echo ""
 echo "Top 3 frequent ERROR messages:"
 
-# Extract error messages and count frequency
 grep "ERROR" "$LOG_FILE" \
     | sed 's/^.*ERROR[: ]*//' \
     | sort \
