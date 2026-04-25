e9a17c2b5092f15fe69a69a9ed43742e294a5216
samarthf-devops <samarthf@valueaddsofttech.com>
2026-04-25 11:59:56 +0530

    Update log_cleanup_automation_5.sh

--- My_Tasks_24_Apr/log_cleanup_automation_5.sh
+++ My_Tasks_24_Apr/log_cleanup_automation_5.sh
@@ -1,3 +1,8 @@
+# Task 5: Log Cleanup Automation
+# Write a script to:
+# - Compress logs older than 2 days
+# - Delete logs older than 7 days
+# .....................................................................
 
 /var/log/myapp/*.log {
     daily
